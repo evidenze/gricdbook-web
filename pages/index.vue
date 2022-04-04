58e0cb09895e8708d505a08bea4dcc725a480983
Essien Ekanem <evidenzeekanem@gmail.com>
2022-04-04 22:27:03 +0100

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -19,12 +19,26 @@
         <p class="pb-3 font-weight-bold">Showing {{ books.length }} result(s)</p>
         <div class="row">
           <div v-for="book in books" :key="book.id" class="col-md-4 mb-3">
-            <div class="card shadow-sm p-4 h-100">
+            <div style="cursor:pointer" class="card shadow-sm p-4 h-100" @click="showDetailsModal(book.id)">
               <img class="mx-auto d-block mb-3 rounded" style="width:100%" :src="`${ $config.apiURL }/${book.image}`">
               <h4 class="font-weight-bold">{{ book.title }}</h4>
               <p class="text-secondary">{{ book.author }}</p>
               <p class="text-secondary">{{ book.published }}</p>
             </div>
+
+            <modal :name="`details-modal${book.id}`" height="auto" :shiftX="1.0" :scrollable="true" :shiftY="1.0">
+              <div class="bg-light p-4">
+                <h4>{{ book.title }}</h4>
+              </div>
+                <div class="container p-5">
+                  <img class="mx-auto d-block mb-3 rounded" style="width:50%" :src="`${ $config.apiURL }/${book.image}`"><br>
+                  <small class="text-secondary">{{ book.description }}</small><br>
+                  <p class="pb-2 mt-5">Author: <span class="badge badge-info">{{ book.author }}</span></p>
+                  <p class="pb-2">Published: <span class="badge badge-info">{{ book.published }}</span></p>
+                  <p class="pb-2">Publisher: <span class="badge badge-info">{{ book.publisher }}</span></p>
+                  <p>ISBN: <span class="badge badge-info">{{ book.isbn }}</span></p>
+                </div>
+            </modal>
           </div>
         </div>
       </div>
@@ -38,7 +52,7 @@
 <script>
   export default {
     layout: 'dashboard',
-    auth: 'guest',
+    auth: false,
       data() {
         return {
           books: [],
@@ -49,6 +63,12 @@
       let books = await this.$axios.get(this.$config.apiURL+'/v1/books');
       this.books = books.data.data.results;
     },
+
+    methods: {
+        showDetailsModal(id) {
+          this.$modal.show('details-modal'+id);
+        },
+      },
 }
 </script>
 
